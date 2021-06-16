17201bb04a6c8d5b294d120e69e8152392d34d34
Russ Carroll <info@rcarroll.dev>
2021-06-15 21:18:12 -0400

    First commit

---
+++ hello-world.py
@@ -0,0 +1,12 @@
+# Import sleep function from time module for sleep command
+from time import sleep
+
+# Print to screen and ask user for name
+print('Good day!')
+name = input('What is your name? ')
+
+# print Hello and the user's name
+print('Hello, ' + name + '!')
+
+# Sleep for 5 seconds
+sleep(5)
